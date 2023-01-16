4dfcb0297cf9a42e8df8fcb41f2019d5847d6d17
nickbahson <nijoba33@gmail.com>
2023-01-16 08:15:34 +0300

    initial payment workflow (basics)

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, redirect
+import utilities
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def app_home():
+
+    # payment form submissions (payment-form name) in html below.
+    if request.method == 'POST' and request.form['amount']:
+        amount = int(request.form['amount'])
+        body = {
+            'amount': amount,
+            'complete_checkout_url': 'http://example.com/complete',
+            'country': 'US',
+            'currency': 'USD',
+            'cancel_checkout_url': 'http://example.com/cancel',
+            'language': 'en',
+        }
+
+        try:
+            # Generate checkout with this payment object
+            req = utilities.make_request('post', '/v1/checkout', body)
+            print('++++++ payment created +++++')
+            print(req['data']['redirect_url'])
+            print('++++++ payment created +++++')
+
+            # Redirect to checkout.
+            return redirect(req['data']['redirect_url'])
+
+        except Exception as ex:
+            print(ex)
+
+    # Checkout html
+    checkout_markup = "<div> <h1>Welcome to my Website!</h1> <hr>" \
+                          "<h1> Checkout my upcoming event</h1> " \
+                          "<section><h3>Consuming 3rd party apis (online)</h3> " \
+                              "<p>Introduction to Postman Collections and api examples</p>" \
+                              "<bold>$100</bold> <p></p>" \
+                              "<form name=payment-form method=POST> " \
+                                    "<input type=hidden name=amount value=100>" \
+                                    " <input type=submit value=\"Reserve your slot \" name=submit>" \
+                              "</form>" \
+                          "</section><hr>" \
+                      "</div>"
+    return checkout_markup
+
+app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+
+### Getting started:
+* Run `pip install -r requirements.txt` to install the dependencies (Flask) the app relies on.
+* Then `python app.py` or `python3 app.py` to start the app @ `http://127.0.0.1:5000/`
